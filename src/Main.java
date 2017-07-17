5a6f34c369205de4138369ca1e80c0193d0b9c1b
ahmadsarvmeily <as20g16@ecs.soton.ac.uk>
2017-07-17 19:32:08 +0100

    -added newVN button functionality, switches to dumb builder interface -3 TODOs with priorities [LOW 2/ HIGH 1]

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,15 @@
 import javafx.application.Application;
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
-/**
- * Created by Ahmad on 17/07/2017.
- */
 public class Main extends Application {
-    //TODO: Launch menu - New VN/Load VN
+
+    private Scene launchMenuScene, builderScene;
 
     public static void main(String[] args) {
         launch(args);
@@ -18,13 +19,41 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("VNBuilder");
 
-        Button newVNButton = new Button("New Visual Novel");
-        //Button loadVNButton = new Button("Load VNBuilder Project");
-
-        StackPane launchMenuLayout = new StackPane(newVNButton);
-        Scene launchMenuScene = new Scene(launchMenuLayout, 800, 600);
+        setupLaunchMenu(primaryStage);
+        setupBuilderMenu(primaryStage);
 
         primaryStage.setScene(launchMenuScene);
         primaryStage.show();
+    }
+
+    private void setupLaunchMenu(Stage stage) {
+        Button newVNButton = new Button("New Visual Novel");
+        newVNButton.setOnAction(e -> {
+            //TODO: [LOW] Open file explorer to choose project root/create project files
+            stage.hide();
+            stage.setScene(builderScene);
+            stage.centerOnScreen();
+            stage.show();
+        });
+
+        Button loadVNButton = new Button("Load VNBuilder Project");
+        //TODO: [LOW] Add loading functionality
+
+        GridPane launchMenuPane = new GridPane();
+        launchMenuPane.setAlignment(Pos.CENTER);
+        launchMenuPane.add(newVNButton,0,0);
+        GridPane.setHalignment(newVNButton, HPos.CENTER);
+        launchMenuPane.add(loadVNButton,0,1);
+        launchMenuPane.setVgap(40);
+
+        launchMenuScene = new Scene(launchMenuPane, 400, 300);
+    }
+
+    private void setupBuilderMenu(Stage stage) {
+        BorderPane rootBuilderPane = new BorderPane();
+
+        //TODO: [HIGH] Create builder menu border elements
+
+        builderScene = new Scene(rootBuilderPane, 800,600);
     }
 }
