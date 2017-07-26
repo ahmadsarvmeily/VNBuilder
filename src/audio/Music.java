ad8f892f7f0a1b98c1fd0c51439a51362a60aa08
ahmadsarvmeily <as20g16@ecs.soton.ac.uk>
2017-07-26 15:25:22 +0100

    removed TODO (no longer necessary), class cleanup

--- src/audio/Music.java
+++ src/audio/Music.java
@@ -6,10 +6,7 @@ import java.io.IOException;
 
 public abstract class Music {
 
-    //TODO: Reduce music volume when sfx are playing, increase afterwards
-
     private static Clip clip;
-    private static FloatControl gainControl;
 
     public static void play(String path) {
 
@@ -19,17 +16,11 @@ public abstract class Music {
             clip = AudioSystem.getClip();
             AudioInputStream inputStream = AudioSystem.getAudioInputStream(new File(path));
             clip.open(inputStream);
-            gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
-            changeVolume(-25.0f);
+            FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+            gainControl.setValue(-25.0f);
             clip.loop(Clip.LOOP_CONTINUOUSLY);
         } catch (LineUnavailableException|UnsupportedAudioFileException|IOException e) {
             e.printStackTrace();
-        }
-    }
-
-    public static void changeVolume(float db) {
-        if(clip != null) {
-            gainControl.setValue(db);
         }
     }
 }
