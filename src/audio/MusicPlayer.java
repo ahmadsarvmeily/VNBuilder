20bbd2199848d86100b775618871982f59afa19a
ahmadsarvmeily <as20g16@ecs.soton.ac.uk>
2017-07-28 19:21:00 +0100

    first music clip plays instantly

--- src/audio/MusicPlayer.java
+++ src/audio/MusicPlayer.java
@@ -1,12 +1,14 @@
 package audio;
 
+import javax.sound.sampled.Clip;
+
 public class MusicPlayer extends VNAudioPlayer {
 
     @Override
     public void play(String path) {
         if(clip != null)
             fadeInto(path,5000);
-        else fadeIn(path,4000);
+        else getClipFromPath(path).loop(Clip.LOOP_CONTINUOUSLY);
     }
 
     public void fadeOut() {
