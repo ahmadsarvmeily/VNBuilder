da81a865a662f47c9cce3a377d3d74b3b38f8d8f
ahmadsarvmeily <as20g16@ecs.soton.ac.uk>
2017-07-30 02:30:30 +0100

    added translucent text box with separate character identifier

--- src/Engine.java
+++ src/Engine.java
@@ -9,10 +9,10 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 import test.EngineTest;
 
@@ -39,10 +39,8 @@ public class Engine extends Application {
 
     private Map<String,ImageView> spriteMap;
 
-    private Pane backgroundPane;
-    private StackPane textPane;
-    private Pane spritePane;
-    private Label text;
+    private Pane backgroundPane, spritePane, characterNamePane;
+    private Label text, characterName;
     private ImageView background;
 
     public static void main(String[] args) {
@@ -60,17 +58,18 @@ public class Engine extends Application {
 
         spriteMap = new HashMap<>();
 
-        BorderPane borderPane = new BorderPane();
+        StackPane rootPane = new StackPane();
+        rootPane.setBackground(new Background(new BackgroundFill(Color.BLACK,CornerRadii.EMPTY,Insets.EMPTY)));
 
         Novel novel = new Novel(novelFile);
 
-        Scene gameScene = new Scene(borderPane, 1600,900);
-
-        setupTextPane();
         setupBackgroundPane();
         setupSpritePane();
-        borderPane.setBottom(textPane);
-        borderPane.setCenter(backgroundPane);
+        setupTextPane();
+        setupCharacterNamePane();
+        rootPane.getChildren().add(backgroundPane);
+
+        Scene gameScene = new Scene(rootPane, 1600,900);
 
         primaryStage.setTitle(novel.getTitle());
         primaryStage.setScene(gameScene);
@@ -122,11 +121,14 @@ public class Engine extends Application {
                     case "fade out music": musicPlayer.fadeOut();
                         continue;
 
-                    case "text": textAnimator.animate(text,"", words[1]);
+                    case "text": characterNamePane.setVisible(false);
+                        textAnimator.animate(text, words[1]);
                         if(words.length == 3) sfxPlayer.play(sfxDir + "/" + words[2]);
                         advance = true;
                         break;
-                    case "speech": textAnimator.animate(text, words[1], words[2]);
+                    case "speech": characterNamePane.setVisible(true);
+                        characterName.setText(words[1]);
+                        textAnimator.animate(text, words[2]);
                         if(words.length == 4) sfxPlayer.play(sfxDir + "/" + words[3]);
                         advance = true;
                         break;
@@ -146,15 +148,34 @@ public class Engine extends Application {
 
     private void setupTextPane() {
         text = new Label("Start game");
-        text.setFont(Font.font(40));
-        text.setPadding(new Insets(0,0,40,40));
-        textPane = new StackPane(text);
+        text.setFont(Font.font(32));
+        text.setWrapText(true);
+        text.setPadding(new Insets(10,0,40,40));
+        text.setTextFill(Color.WHITE);
+        StackPane textPane = new StackPane(text);
+        textPane.setBackground(new Background(new BackgroundFill(Color.color(0,0,0,0.7), new CornerRadii(10), new Insets(0,0,0,0))));
+        textPane.setLayoutY(750);
+        textPane.setPrefSize(1600,150);
         textPane.setAlignment(Pos.TOP_LEFT);
+        spritePane.getChildren().add(textPane);
+    }
+
+    private void setupCharacterNamePane() {
+        characterName = new Label();
+        characterName.setFont(Font.font("Arial", FontWeight.SEMI_BOLD,36));
+        characterName.setPadding(new Insets(0,25,0,25));
+        characterName.setTextFill(Color.WHITE);
+        characterNamePane = new Pane(characterName);
+        characterNamePane.setBackground(new Background(new BackgroundFill(Color.color(0,0,0,0.7), new CornerRadii(10), new Insets(0,0,0,0))));
+        characterNamePane.setLayoutX(5);
+        characterNamePane.setLayoutY(705);
+        characterNamePane.setVisible(false);
+        spritePane.getChildren().add(characterNamePane);
     }
 
     private void setupBackgroundPane() {
         background = new ImageView();
-        background.setFitHeight(750);
+        background.setFitHeight(900);
         background.setFitWidth(1600);
         backgroundPane = new Pane(background);
     }
