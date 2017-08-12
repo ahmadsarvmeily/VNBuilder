13c590f43c156f8d13b81df087c06be8250bad5a
ahmadsarvmeily <as20g16@ecs.soton.ac.uk>
2017-08-12 13:34:12 +0100

    added final gain parameter to allow fading into current config volume level

--- src/audio/FadeIn.java
+++ src/audio/FadeIn.java
@@ -8,19 +8,20 @@ public class FadeIn extends Thread {
     private Clip clip;
     private FloatControl gainControl;
     private int durationMillis, loops;
+    private float finalGain;
 
-    FadeIn(Clip clip, int durationMillis, int loops) {
+    FadeIn(Clip clip, int durationMillis, float volume, int loops) {
         this.clip = clip;
         gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
         gainControl.setValue(-80.0f);
         this.durationMillis = durationMillis;
+        this.finalGain = 20f * (float) Math.log10(volume);
         this.loops = loops;
     }
 
     @Override
     public void run() {
         final float initialGain = -80.0f;
-        final float finalGain = 0.0f;
         final float dGain = Math.abs(finalGain-initialGain);
 
         clip.loop(loops);
