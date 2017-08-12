446a3ae3935b72012cd2119a2cf36ffbe6f348c9
ahmadsarvmeily <as20g16@ecs.soton.ac.uk>
2017-08-12 13:32:02 +0100

    moved play function to superclass

--- src/audio/SfxPlayer.java
+++ src/audio/SfxPlayer.java
@@ -8,14 +8,6 @@ public class SfxPlayer extends VNAudioPlayer {
         sfxPlayer = this;
     }
 
-    @Override
-    public void play(String path) {
-        if(clip != null)
-            clip.stop();
-        clip = getClipFromPath(path);
-        clip.start();
-    }
-
     public static SfxPlayer getInstance() {
         if(sfxPlayer == null) {
             sfxPlayer = new SfxPlayer();
