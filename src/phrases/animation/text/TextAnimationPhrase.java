1a2bae0b5cc936a068e513bbf01f9b45e93d404c
ahmadsarvmeily <as20g16@ecs.soton.ac.uk>
2017-08-12 13:27:39 +0100

    removed general audio playback to allow separate channels for speech and sfx

--- src/phrases/animation/text/TextAnimationPhrase.java
+++ src/phrases/animation/text/TextAnimationPhrase.java
@@ -1,26 +1,21 @@
 package phrases.animation.text;
 
 import animation.TextAnimator;
-import audio.SfxPlayer;
 import engine.ui.VNEngineUI;
 import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import engine.Directories;
-import engine.Engine;
 import phrases.Phrase;
 
 public abstract class TextAnimationPhrase extends Phrase {
 
     private TextAnimator textAnimator;
     private Label textLabel;
-    String text, sfxFileName;
+    String text, sfxFileName, sfxDir;
     boolean playAudio;
-    private SfxPlayer sfxPlayer;
-    private String sfxDir;
     Pane characterNamePane;
     private VBox textLogContentPane;
     Label textHistoryLabel;
@@ -28,7 +23,6 @@ public abstract class TextAnimationPhrase extends Phrase {
     TextAnimationPhrase() {
         playAudio = false;
         textAnimator = TextAnimator.getInstance();
-        sfxPlayer = SfxPlayer.getInstance();
         sfxDir = Directories.getSfxDir();
         characterNamePane = VNEngineUI.getSpeakerNamePane();
         textLabel = VNEngineUI.getTextPane().getLabel();
@@ -44,9 +38,5 @@ public abstract class TextAnimationPhrase extends Phrase {
         textHistoryLabel.setTextFill(Color.color(0.8,0.8,0.8));
         textHistoryLabel.setFont(Font.font("Arial Narrow",32));
         textLogContentPane.getChildren().add(textHistoryLabel);
-
-        if(playAudio) {
-            sfxPlayer.play(sfxDir + "/" + sfxFileName);
-        }
     }
 }
