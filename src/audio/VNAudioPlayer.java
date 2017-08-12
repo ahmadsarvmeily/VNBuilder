53257488d8614f9e185d724c19e8ad584768d696
ahmadsarvmeily <as20g16@ecs.soton.ac.uk>
2017-08-12 13:31:15 +0100

    changed volume scale from logarithmic to linear (0 = silent, 1 = full volume)

--- src/audio/VNAudioPlayer.java
+++ src/audio/VNAudioPlayer.java
@@ -4,11 +4,19 @@ import javax.sound.sampled.*;
 import java.io.File;
 import java.io.IOException;
 
-abstract class VNAudioPlayer {
+abstract public class VNAudioPlayer {
 
+    // 0.0 = silent, 1.0 = full volume
+    float volume = 0.5f;
     Clip clip;
 
-    public abstract void play(String path);
+    public void play(String path) {
+        if(clip != null)
+            clip.stop();
+        clip = getClipFromPath(path);
+        setVolume(volume);
+        clip.start();
+    }
 
     static Clip getClipFromPath(String path) {
         Clip clip = null;
@@ -25,22 +33,25 @@ abstract class VNAudioPlayer {
 
     void fadeInto(String path, int durationMillis) {
         Clip into = getClipFromPath(path);
-        new FadeInto(clip,into,durationMillis,Clip.LOOP_CONTINUOUSLY).start();
+        new FadeInto(clip,into,durationMillis,volume,Clip.LOOP_CONTINUOUSLY).start();
         clip = into;
     }
 
     void fadeIn(String path, int durationMillis) {
         Clip in = getClipFromPath(path);
         clip = in;
-        new FadeIn(in,durationMillis,Clip.LOOP_CONTINUOUSLY).start();
+        new FadeIn(in,durationMillis,volume,Clip.LOOP_CONTINUOUSLY).start();
     }
 
     void fadeOut(int durationMillis) {
         new FadeOut(clip,durationMillis).start();
     }
 
-    void setVolume(float db) {
-        FloatControl gainControl = (FloatControl)clip.getControl(FloatControl.Type.MASTER_GAIN);
-        gainControl.setValue(db);
+    public void setVolume(float volume) {
+        this.volume = volume;
+        if(clip != null) {
+            FloatControl volumeControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+            volumeControl.setValue(20f * (float) Math.log10(volume));
+        }
     }
 }
